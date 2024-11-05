122863e2d273c77a6986499791120cd647277d96
Murray Christian <murraychristian@live.co.za>
2024-11-05 15:00:55 +0200

    refactored sim_rep_cmr_data() into sim_cmr_data() and get_pars_from_posterior()

--- R/01b_fn_sim_rep_data_multiage_multisite_trap-dep.R
+++ R/01b_fn_sim_rep_data_multiage_multisite_trap-dep.R
@@ -1,28 +1,36 @@
-# Function to simulate a replicate dataset under model under the multi-age,
-# multi-site model '01_multiage_multisite_trap-dep.stan' that incorporates trap-
-# dependence
+#' Simulate cmr data under the multi-age, multi-site model '01_multiage_multisite_trap-dep.stan'
+#' that incorporates trap-dependence (but not transience).
+#' The code is factored so that data can be simulated under the model either
+#'  (i) using parameter values from a posterior draw of the fit model and the 
+#'  structure of the dataset to which it was fit (for posterior predictive checks),
+#'  or
+#' (ii) user-specified parameter values and compatible dataset structure (useful
+#' when simulation-testing the model) 
 library(tidyverse)
 
-#' The function arguments are:
-#' 'fit' - a cmdstanr model fit of '01_multiage_multisite_trap-dep.stan' to
-#'   the data 'data/00b_cmr_data_multisite_multiage_trapdep.RDS'
-#' 'draw' - a posterior sample to use for the data-generating parameter values
-#' 'data_str' - a list of dataset structure attributes, including study length,
-#'   total number of individuals, and a year x age x site array whose (i,j,k)
-#'   entry is the number of individuals first captured in year i, of age-class j
-#'   at site k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ---- Function to sim data under '01_multiage_multisite_trap-dep.stan' ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-sim_rep_cmr_data <- function( fit, draw, data_str ) {
+#' The function arguments are:
+#' 'pars' - a list of matrices of parameter values 
+#' 'data_str' - a list of dataset structure attributes, including study length,
+#'   total number of individuals, a vector of first capture occasions, and a 
+#'   vector of codes at first capture (which encode age and site information)
+
+sim_cmr_data <- function(pars, data_str) {
   
-  # make sure posterior draw is available
-  if (draw > nrow(fit$draws(format="df"))) {
-    stop(str_c("Draw ", draw ," requested but only ",
-               nrow(fit$draws(format="df"))," draws available!"))
+  # check that all parameters required are present
+  pars_required <- c("phi_ad", "phi_jv", "p_im", "p_ad_A", "p_ad_U", "p_ad_N",
+                     "m_jv", "m_ad")
+  if (!setequal(names(pars), pars_required)) {
+    stop(str_c("Error! Required parameters missing or unknown parameters given: ",
+               str_flatten_comma(pars_required), " are required."))
   }
   
-  # unpack dataset structure
-  T <-data_str$T
+  # decant dataset structure
   N <- data_str$N
+  T <- data_str$T
   fc <- data_str$fc
   fc_code <- data_str$fc_code
   
@@ -34,44 +42,41 @@ sim_rep_cmr_data <- function( fit, draw, data_str ) {
     print("Warning: removed individuals first captured on last capture occasion!")
   }
   
-  # format posterior sample
-  vars <- c("phi_jv", "phi_ad", "p_im", "p_ad_A", "p_ad_U", "p_ad_N", "m_jv", "m_ad")
-  post_sample <- fit$draws(vars, format="df") %>%
-    filter(.draw==draw) %>%
-    select(-starts_with("."))
-  
-  # decant values to matrices 
-  extract_matrix <- function(post_sample, par) {
-    temp <- post_sample %>%
-      select(starts_with(par)) %>%
-      pivot_longer(everything()) %>%
-      mutate(
-        site = as.integer(str_extract(name, "(?<=\\[)[1-9](?=,)"))
-      )
-    mat <- lapply(1:3, function(i) filter(temp, site==i)$value) %>%
-      do.call(rbind,.)
-    mat
+  # check that parameters have the correct dimensions
+  if (any(dim(pars$phi_ad)!=c(3,T-1))) {
+    stop(str_c("Error! Incorrect dimensions for 'phi_ad'; should be 3 by ",T-1,"."))
+  }
+  if (any(dim(pars$phi_jv)!=c(3,T-1))) {
+    stop(str_c("Error! Incorrect dimensions for 'phi_jv'; should be 3 by ",T-1,"."))
+  }
+  if (any(dim(pars$p_im)!=c(3,T))) {
+    stop(str_c("Error! Incorrect dimensions for 'p_im'; should be 3 by ",T,"."))
+  }
+  if (any(dim(pars$p_ad_A)!=c(3,T))) {
+    stop(str_c("Error! Incorrect dimensions for 'p_ad_A'; should be 3 by ",T,"."))
+  }
+  if (any(dim(pars$p_ad_U)!=c(3,T))) {
+    stop(str_c("Error! Incorrect dimensions for 'p_ad_U'; should be 3 by ",T,"."))
+  }
+  if (any(dim(pars$p_ad_N)!=c(3,T))) {
+    stop(str_c("Error! Incorrect dimensions for 'p_ad_N'; should be 3 by ",T,"."))
+  }
+  if (any(dim(pars$m_jv)!=c(3,3))) {
+    stop("Error! Incorrect dimensions for 'm_jv'; should be 3 by 3.")
+  }
+  if (any(dim(pars$m_ad)!=c(3,3))) {
+    stop("Error! Incorrect dimensions for 'm_ad'; should be 3 by 3.")
   }
   
-  # parking this simpler but possibly less safe version of extract_matrix() here
-  # extract_matrix <- function(post_sample, par) {
-  #   post_sample %>%
-  #     select(starts_with(par)) %>%
-  #     pivot_longer( everything() ) %>%
-  #     pull(value) %>%
-  #     matrix(nrow=3)
-  # }
-  
-  # site x year matrices for survival and detection
-  phi_jv <- extract_matrix(post_sample, "phi_jv")
-  phi_ad <- extract_matrix(post_sample, "phi_ad")
-  p_im <- extract_matrix(post_sample, "p_im")
-  p_ad_A <- extract_matrix(post_sample, "p_ad_A")
-  p_ad_U <- extract_matrix(post_sample, "p_ad_U")
-  p_ad_N <- extract_matrix(post_sample, "p_ad_N")
-  #  source site x destinatino site matrices for movement
-  m_jv <- extract_matrix(post_sample, "m_jv")
-  m_ad <- extract_matrix(post_sample, "m_ad")
+  # decant parameters
+  phi_jv <- pars$phi_jv
+  phi_ad <- pars$phi_ad
+  p_im <- pars$p_im
+  p_ad_A <- pars$p_ad_A
+  p_ad_U <- pars$p_ad_U
+  p_ad_N <- pars$p_ad_N
+  m_jv <- pars$m_jv
+  m_ad <- pars$m_ad
   
   # create transition matrices 
   trans <- list()
@@ -125,7 +130,7 @@ sim_rep_cmr_data <- function( fit, draw, data_str ) {
     # enforce row-sum-to-one constraint
     obs[[t]][,13] = 1 - rowSums(obs[[t]][,1:12])
   }
-
+  
   # initialise z matrix of states
   z <- matrix(0, nrow = N, ncol = T)
   for (t in 1:(T-1)) {
@@ -150,4 +155,53 @@ sim_rep_cmr_data <- function( fit, draw, data_str ) {
   # return
   list(fc=fc, fc_code=fc_code, z=z, y=y)
 }
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#         --- Function to format a posterior draw for sim_cmr_data() ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# function arguments:
+# 'fit' - a cmdstanr fit of model '01_multiage_multisite_trap-dep.stan'
+# 'draw' - a positive integer (usually between 1 and 4000) 
+
+get_pars_from_posterior <- function( fit, draw ) {
+  # make sure posterior draw is available
+  if (draw > nrow(fit$draws(format="df"))) {
+    stop(str_c("Draw ", draw ," requested but only ",
+               nrow(fit$draws(format="df"))," draws available!"))
+  }
   
+  # format posterior sample
+  vars <- c("phi_jv", "phi_ad", "p_im", "p_ad_A", "p_ad_U", "p_ad_N", "m_jv", "m_ad")
+  post_sample <- fit$draws(vars, format="df") %>%
+    filter(.draw==draw) %>%
+    select(-starts_with("."))
+  
+  # decant values to matrices 
+  extract_matrix <- function(post_sample, par) {
+    temp <- post_sample %>%
+      select(starts_with(par)) %>%
+      pivot_longer(everything()) %>%
+      mutate(
+        site = as.integer(str_extract(name, "(?<=\\[)[1-9](?=,)"))
+      )
+    mat <- lapply(1:3, function(i) filter(temp, site==i)$value) %>%
+      do.call(rbind,.)
+    mat
+  }
+  
+  # package as list and return
+  pars_list <- lapply( vars, function(par) extract_matrix(post_sample, par) )
+  names(pars_list) <- vars
+  pars_list
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                 ---- Wrapper for convenience ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sim_rep_cmr_data <- function(fit, draw, data_str) {
+  pars <- get_pars_from_posterior(fit, draw)
+  sim_cmr_data(pars, data_str)
+}
+
