fe1344f6a447428454632c62ff154d3f3a5371a0
Murray Christian <murraychristian@live.co.za>
2024-11-26 11:35:57 +0200

    removed 2 x 2 restriction on contingency table in get_expected_frequencies(), will now run with any dimension m-by-n

--- R/00_function_get_expected_frequencies.R
+++ R/00_function_get_expected_frequencies.R
@@ -1,17 +1,17 @@
-#' Get expected frequencies for 2x2 contingency tables under null hypothesis
+#' Get expected frequencies for contingency tables under null hypothesis
 #' of no difference between the rows (i.e. a test of homogeneity).
 
 get_expected_frequencies <- function(observed) {
-  # check dimensions
-  if (!(all(dim(observed) == c(2,2)))) {
-    stop("Contingency tables must be 2 x 2.")
-  }
-  row_totals <- matrix( rowSums(observed), nrow = 2, ncol = 1) 
-  col_totals <- matrix( colSums(observed), nrow = 1, ncol = 2)
+  # size of table
+  m <- nrow(observed)
+  n <- ncol(observed)
   
-  # check for zero table 
+  row_totals <- matrix( rowSums(observed), nrow = m, ncol = 1) 
+  col_totals <- matrix( colSums(observed), nrow = 1, ncol = n)
+  
+  # if observed is all zeros, set expected the same 
   if (sum(col_totals) == 0) {
-    expected <- matrix(0, nrow = 2, ncol = 2)
+    expected <- matrix(0, nrow = m, ncol = n)
     rownames(expected) <- rownames(observed)
     colnames(expected) <- colnames(observed)
   }
@@ -21,7 +21,7 @@ get_expected_frequencies <- function(observed) {
     rownames(expected) <- rownames(observed)
     colnames(expected) <- colnames(observed)
   }
-
+  
   # return
   expected
 }
