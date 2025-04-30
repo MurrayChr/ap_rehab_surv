0f5284f4505f149301a4a764ff27ac3fef401709
Murray Christian <murraychristian@live.co.za>
2025-04-30 12:49:09 +0200

    created multi-age, multi-site mark-recapture data without trap-dependence states

--- R/00b_create_cmr_data.R
+++ R/00b_create_cmr_data.R
@@ -142,6 +142,80 @@ bird_year_site <- left_join( bird_year_site, marking_sites, by="primary_id" ) %>
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #       ---- Create multi-age, multi-site mark-recapture data ----
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Here we create mark-recapture data for a multi-age, multi-site model (without
+#' trap-dependence)
+ 
+# encode age_class and site (assigned site) information 
+bird_year_site <- bird_year_site %>%
+  mutate(
+    code = case_when(
+      age_class==1 & assigned_site=="Robben" ~ 1,
+      age_class==2 & assigned_site=="Robben" ~ 2,
+      age_class==3 & assigned_site=="Robben" ~ 3,
+      age_class==1 & assigned_site=="Boulders" ~ 4,
+      age_class==2 & assigned_site=="Boulders" ~ 5,
+      age_class==3 & assigned_site=="Boulders" ~ 6,
+      age_class==1 & assigned_site=="Stony" ~ 7,
+      age_class==2 & assigned_site=="Stony" ~ 8,
+      age_class==3 & assigned_site=="Stony" ~ 9
+    )
+  )
+
+# Note: this encoding is specific to the model we will use to analyse the data,
+# for a different model, a different encoding will usually be required
+
+# pivot wider to mark-recapture dataset 
+cmr_data <- bird_year_site %>%
+  select( primary_id, year, code, mark_year, marking_age, marking_site ) %>%
+  group_by( primary_id ) %>%
+  pivot_wider(
+    names_from = year, names_prefix = "yr", names_sort = TRUE,
+    values_from = code, values_fill = 0
+  ) %>% 
+  ungroup() %>% 
+  arrange( mark_year, marking_age, marking_site, primary_id )
+
+# add first capture 
+cmr_data <- cmr_data %>%
+  rowwise() %>%
+  mutate( 
+    fc = min( which( c_across( starts_with("yr") )!=0 ) ),
+    .after = marking_site
+  ) %>%
+  ungroup()
+
+# uncomment to check that mark_year and first capture agree; assuming first year 
+# is 2013, this should return TRUE:
+# all(cmr_data$fc + 2013 - 1 == cmr_data$mark_year)
+
+# add code at first capture 
+# (used when simulating replicate data for posterior predictive checks)
+cmr_data <- cmr_data %>%
+  rowwise() %>%
+  mutate(
+    fc_code = c_across(starts_with("yr"))[fc],
+    .after=fc
+  )
+
+##  Add hand-rearing information, checks, and save 
+##
+
+peng <- readRDS("data/penguins.RDS")
+cmr_data <- left_join( cmr_data, peng, by="primary_id" ) %>%
+  relocate( hr, .after=marking_age )
+
+# check everyone has hand-reared information; following should return TRUE
+# all(!is.na(cmr_data$hr))
+
+# check hand-reared birds are all birds marked as chicks; should return TRUE
+# all( filter(cmr_data, hr==1)$marking_age == 1)
+
+# save
+# saveRDS(cmr_data, "data/00b_cmr_data_multisite_multiage.RDS")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#       ---- Create multi-age, multi-site, trap-dep mark-recapture data ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #' Here we create mark-recapture data for a multi-age, multi-site model that
 #' that incorporates trap-dependence in the adult age class at each colony.
 
@@ -200,9 +274,9 @@ cmr_data <- cmr_data %>%
     .after=fc
   )
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#          ---- Add hand-rearing information, checks, save ----
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##  Add hand-rearing information, checks, and save 
+##
+
 peng <- readRDS("data/penguins.RDS")
 cmr_data <- left_join( cmr_data, peng, by="primary_id" ) %>%
   relocate( hr, .after=marking_age )
