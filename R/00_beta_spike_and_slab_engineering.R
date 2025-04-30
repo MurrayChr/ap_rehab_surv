c8ecda371a60a96e0ac3a23d09dac615a1de01a5
Murray Christian <murraychristian@live.co.za>
2025-04-30 17:41:27 +0200

    model with trap-dep, hand-rearing, immature survival and transients with a spike-and-slab prior

---
+++ R/00_beta_spike_and_slab_engineering.R
@@ -0,0 +1,52 @@
+# Engineering a 'spike-and-slab' prior for probability parameters as a mixture
+# of beta distributions. We dub the result a 'beta-spike-and-slab' prior, or
+# 'bss' prior for short. 
+library(tidyverse)
+
+# Compare histograms and analytically derived density
+alpha_for_spike <- 999
+beta_for_spike <- 1
+alpha_for_slab <- 2
+beta_for_slab <- 1
+slab_weight <- 5 # 'alpha' for mixture distribution
+spike_weight <- 25 # 'beta' for mixture distribution
+
+# Large sample
+N <- 1e4
+y <- rep(NA, N)
+for (i in 1:N) {
+  rho <- rbeta(1,spike_weight, slab_weight)
+  zeta <- rbinom(1,1,rho)
+  if (zeta == 1) {
+    y[i] <- rbeta(1, alpha_for_spike, beta_for_spike)
+  }
+  if (zeta == 0) {
+    y[i] <- rbeta(1, alpha_for_slab, beta_for_slab)
+  }
+}
+
+hist(y, probability = TRUE, xlim = c(0,1), breaks = seq(0,1,length.out = 101))
+curve(
+  (spike_weight / (slab_weight + spike_weight)) * dbeta(x, alpha_for_spike, beta_for_spike) +
+    (slab_weight / (slab_weight + spike_weight)) * dbeta(x, alpha_for_slab, beta_for_slab),
+  0, 1, add = TRUE, lwd = 2
+)
+
+# Moderate sample
+N <- 30
+y <- rep(NA, N)
+for (i in 1:N) {
+  rho <- rbeta(1,spike_weight, slab_weight)
+  zeta <- rbinom(1,1,rho)
+  if (zeta == 1) {
+    y[i] <- rbeta(1, alpha_for_spike, beta_for_spike)
+  }
+  if (zeta == 0) {
+    y[i] <- rbeta(1, alpha_for_slab, beta_for_slab)
+  }
+}
+
+hist(y, xlim = c(0,1), breaks = seq(0,1,length.out = 41), ylim = c(0,30))
+
+
+
---
+++ R/06a_fit_trap-dep_trans-bss_hr_phi-im.R
@@ -0,0 +1,176 @@
+#' Fit model with trap-dependence, transience and additive hand-rearing effect 
+#' on survival which additionally includes 
+#'   (i) separate immature survival (as opposed to them sharing survival with adults)
+#'   (ii) a spike-and-slab prior on residency probabilities (for details see 
+#'        00_beta_spike_and_slab_engineering.R)
+library(tidyverse)
+library(cmdstanr)
+source("R/00_function_get_marray.R")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#           ---- Load cmr data and format into m-arrays ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# load data
+cmr_data <- readRDS("data/00b_cmr_data_multisite_multiage_trapdep.RDS")
+
+# set number of years
+T <- sum(str_starts(colnames(cmr_data),"yr"))
+
+# add last capture occasion to find individuals not recaptured after first capture
+cmr_data <- cmr_data %>%
+  rowwise() %>%
+  mutate( 
+    lc = max( which( c_across( starts_with("yr") )!=0 ) ),
+    .after = fc
+  ) %>%
+  ungroup()
+
+# split data into three parts
+cmr_data_not_for_marr <- cmr_data %>%
+  filter(
+    (marking_age == 3) & (fc == lc)
+  )
+cmr_data_for_marr_wr <- cmr_data %>%
+  filter(
+    !((marking_age == 3) & (fc == lc)) & (hr == 0)
+  )
+cmr_data_for_marr_hr <- cmr_data %>%
+  filter(
+    !((marking_age == 3) & (fc == lc)) & (hr == 1)
+  )
+
+# check that these add up
+# nrow(cmr_data_for_marr_hr) + nrow(cmr_data_for_marr_wr) +
+#   nrow(cmr_data_not_for_marr) == nrow(cmr_data)
+
+# check that there are no marking_age == 3 in cmr_data_for_marr_hr; all
+# hand-reared birds should be marked as juveniles, not adults
+# all(cmr_data_for_marr_hr$marking_age != 3)
+
+# prepare data for get_marray()
+prepare_cmr_data <- function(cmr_data_for_marr) {
+  # split into capture history matrix and first-capture vector
+  y <- cmr_data_for_marr %>%
+    select( starts_with("yr") ) %>%
+    as.matrix()
+  fc <- cmr_data_for_marr$fc
+  
+  # change '0's after first capture to nStates + 1 = 13 for get_marray()
+  for (i in 1:nrow(y)) {
+    y[i,fc[i]:T][y[i, fc[i]:T]==0] <- 13 
+  }
+  
+  # return
+  y
+}
+y_wr <- prepare_cmr_data(cmr_data_for_marr_wr)
+y_hr <- prepare_cmr_data(cmr_data_for_marr_hr)
+
+# m-array for model '02_multiage_multisite_trap-dep_trans.stan' which has 12 states
+# states 4,8,12 are unobservable so the warning about their absence can be safely ignored
+marr_wr <- get_marray(y_wr, nStates=12)
+marr_hr <- get_marray(y_hr, nStates=12)
+
+# count number of birds marked as adults that are ever/never recaptured
+# per site and cohort
+N_1 <- matrix(NA, 3, T-1)
+N_0 <- matrix(NA, 3, T-1)
+for (i in 1:3) {
+  for (t in 1:(T-1)) {
+    N_1[i,t] <- cmr_data_for_marr_wr %>%
+      filter( fc_code==(3+4*(i-1)) & fc==t ) %>%
+      nrow()
+    N_0[i,t] <- cmr_data_not_for_marr %>%
+      filter( fc_code==(3+4*(i-1)) & fc==t ) %>%
+      nrow()
+  }
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#                      ---- Fit the model ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# compile and fit (~ 20 mins)
+file <- "stan/06_td_tr-bss_hr_phi-im.stan"
+mod <- cmdstan_model(file)
+stan_data <- list(T=T, marr_wr=marr_wr, marr_hr = marr_hr, N_1=N_1, N_0=N_0)
+fit <- mod$sample(stan_data, parallel_chains = 4)
+fit$save_object("outputs/06a_td_tr-bss_hr_phi-im_fit.RDS")
+
+# diagnostic summary
+fit$diagnostic_summary()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#               ---- Plot estimates ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+# load fitted model object 
+fit <- readRDS("outputs/06a_td_tr-bss_hr_phi-im_fit.RDS")
+
+# posteriors for hand-rearing coefficient (logit scale)
+fit$draws(c("hr_jv", "hr_ad"), format = "df") %>%
+  select(-starts_with(".")) %>%
+  pivot_longer(everything()) %>%
+  ggplot(aes(x=value, fill=name)) +
+  geom_density(alpha = 0.5) +
+  coord_cartesian(xlim = c(-1,1)) +
+  geom_vline(xintercept = 0, colour="grey", linetype = "dotted") +
+  scale_fill_manual(values=c("darkorange", "navyblue"), labels = c("adult", "juvenile"), name = "") +
+  theme_classic() +
+  theme(legend.position = "inside", legend.position.inside = c(0.7,0.85)) +
+  labs(
+    # title = "Age-specific hand-rearing effects",
+    x = "Hand-rearing coefficient (logit scale)", 
+    y = "Posterior density" 
+  )
+
+
+# immature survival
+fit$summary("phi_im") %>%
+  mutate(
+    par = str_extract(variable, "[a-z_]+(?=\\[)"),
+    t = as.integer(str_extract(variable,"(?<=\\[)[0-9]+(?=\\])")),
+  ) %>%
+  ggplot(aes(x = t)) +
+  geom_pointrange(aes(y=median, ymin = q5, ymax = q95)) +
+  scale_x_continuous(breaks = 1:10) +
+  coord_cartesian(ylim=c(0,1)) +
+  theme_classic() +
+  theme(
+    panel.grid.major = element_line(colour = "grey95")
+  ) +
+  labs(
+    x = "Year", y = "Posterior median and 90% CrI", 
+    title = "Immature survival",
+    subtitle = "Shared between sites and hand-rearing categories, with year random effect"
+  )
+
+# residency probabilities
+fit$summary("pi_r") %>%
+  mutate(
+    site_index = as.integer( str_extract(variable, "(?<=\\[)[1-9](?=,)")  ),
+    t = as.integer( str_extract(variable,"(?<=,)[0-9]+(?=\\])") ),
+    year = 2012 + t,
+    site = case_when(
+      site_index == 1 ~ "Robben",
+      site_index == 2 ~ "Boulders",
+      site_index == 3 ~ "Stony"
+    ),
+    xshift = 0.1*case_when(
+      site_index == 1 ~ -1,
+      site_index == 2 ~ 0,
+      site_index == 3 ~ 1
+    )
+  ) %>%
+  ggplot( aes(x=year+xshift, colour=site) ) +
+  geom_pointrange( aes(y=median, ymin=q5, ymax=q95), size=0.8, linewidth=0.8 ) +
+  scale_x_continuous( breaks=2013:2023 ) +
+  coord_cartesian( ylim=c(0,1) ) +
+  theme_classic() +
+  theme(
+    panel.grid.major = element_line(),
+    legend.position.inside = TRUE,
+    legend.position = c(0.75, 0.25)
+  ) + 
+  labs( x= "year", y="estimate", title = "Residency probability")
+
